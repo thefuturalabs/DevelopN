2234fad406d9184f892ead58c561a01edf0e6aa7
shanu <shanu.futura@gmail.com>
2023-02-25 15:35:39 +0530

    getting company profile

--- api/user_profile_view.php
+++ api/user_profile_view.php
@@ -2,13 +2,13 @@
 include '../Login_v1/connection.php';
 
 $user_id = $_POST['user_id'];
-$data = mysqli_query($con,"select * from providers where login_id = '$user_id' ");
+$data = mysqli_query($con,"select * from user where login_id = '$user_id' ");
 $row = mysqli_fetch_assoc($data);
 
 $myarray['name'] = $row['name'];
 $myarray['email'] = $row['email'];
-$myarray['mobile'] = $row['phn_no'];
-$myarray['dp'] = $row['image'];
+$myarray['mobile'] = $row['mobile'];
+$myarray['qualification'] = $row['qualification'];
 
 echo json_encode($myarray);
 ?>
